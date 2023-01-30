484e5eae5935ac8b328455b452f59d5c5b9c653c
MikeHatchi <mikehatchi@hotmail.com>
2023-01-30 18:32:24 +0100

    1st commit

---
+++ classes/base.py
@@ -0,0 +1,8 @@
+# screen dimension
+width = 800
+height = 800
+
+# chess board
+cols = 8
+rows = 8
+sqsize = width // cols
---
+++ classes/main.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Main:
+
+    def __init__(self):
+        pass
+
+    def mainloop(self):
+        pass
+
+
+main = Main()
+main.mainloop()
