921e5a41c50900c84954566a773b005f27085202
MikeHatchi <mikehatchi@hotmail.com>
2023-02-16 13:17:40 +0100

    test func for nn

--- src/model_src/magic_chess.py
+++ src/model_src/magic_chess.py
@@ -1,5 +1,7 @@
 from dataset import ZDataset
 
+from cnn_model import Net
+
 model = Net()
 
 if train_on_gpu:
--- src/model_src/nn_train.py
+++ src/model_src/nn_train.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
 from torch import optim
-import torch.nn.functional as F
 import numpy as np 
 
+"""
+ASCII SET isometric1 http://asciiset.com/figletserver.html
+"""
 # CUDA's availability
 train_on_gpu = torch.cuda.is_available()
 
@@ -37,24 +39,32 @@ def validation(model, testloader, criterion):
 
 def train(model, epochs=5):
 
-
-
     # loss function
     criterion = nn.CrossEntropyLoss() # To Change
 
     # optimizer
     optimizer = optim.Adam(model.parameters(), lr = 0.01)
 
-    
+    valid_loss_min = np.Inf # track change in validation loss
+
     for e in range(epochs):
 
         train_loss = 0
         valid_loss = 0
 
-        ######################
-        #_|_  ._   _.  o  ._  
-        # |_  |   (_|  |  | | 
-        ######################
+
+        #      ___           ___           ___                       ___     
+        #     /\  \         /\  \         /\  \          ___        /\__\    
+        #     \:\  \       /::\  \       /::\  \        /\  \      /::|  |   
+        #      \:\  \     /:/\:\  \     /:/\:\  \       \:\  \    /:|:|  |   
+        #      /::\  \   /::\~\:\  \   /::\~\:\  \      /::\__\  /:/|:|  |__ 
+        #     /:/\:\__\ /:/\:\ \:\__\ /:/\:\ \:\__\  __/:/\/__/ /:/ |:| /\__\
+        #    /:/  \/__/ \/_|::\/:/  / \/__\:\/:/  / /\/:/  /    \/__|:|/:/  /
+        #   /:/  /         |:|::/  /       \::/  /  \::/__/         |:/:/  / 
+        #   \/__/          |:|\/__/        /:/  /    \:\__\         |::/  /  
+        #                  |:|  |         /:/  /      \/__/         /:/  /   
+        #                   \|__|         \/__/                     \/__/    
+
 
         # model in training mode, dropout is on
         model.train()
@@ -78,10 +88,19 @@ def train(model, epochs=5):
             train_loss += loss.item()*data.size(0)
 
 
-        ##################################
-        #     _.  |  o   _|   _.  _|_   _  
-        #\/  (_|  |  |  (_|  (_|   |_  (/_ 
-        ##################################
+
+        #        ___           ___           ___                   ___           ___           ___           ___     
+        #       /\__\         /\  \         /\__\      ___        /\  \         /\  \         /\  \         /\  \    
+        #      /:/  /        /::\  \       /:/  /     /\  \      /::\  \       /::\  \        \:\  \       /::\  \   
+        #     /:/  /        /:/\:\  \     /:/  /      \:\  \    /:/\:\  \     /:/\:\  \        \:\  \     /:/\:\  \  
+        #    /:/__/  ___   /::\~\:\  \   /:/  /       /::\__\  /:/  \:\__\   /::\~\:\  \       /::\  \   /::\~\:\  \ 
+        #    |:|  | /\__\ /:/\:\ \:\__\ /:/__/     __/:/\/__/ /:/__/ \:|__| /:/\:\ \:\__\     /:/\:\__\ /:/\:\ \:\__\
+        #    |:|  |/:/  / \/__\:\/:/  / \:\  \    /\/:/  /    \:\  \ /:/  / \/__\:\/:/  /    /:/  \/__/ \:\~\:\ \/__/
+        #    |:|__/:/  /       \::/  /   \:\  \   \::/__/      \:\  /:/  /       \::/  /    /:/  /       \:\ \:\__\  
+        #     \::::/__/        /:/  /     \:\  \   \:\__\       \:\/:/  /        /:/  /     \/__/         \:\ \/__/  
+        #      ~~~~           /:/  /       \:\__\   \/__/        \::/__/        /:/  /                     \:\__\    
+        #                     \/__/         \/__/                 ~~            \/__/                       \/__/    
+
                     
         model.eval()
 
@@ -96,3 +115,58 @@ def train(model, epochs=5):
             loss = criterion(output, target)
             # train_loss update
             valid_loss += loss.item()*data.size(0)
+
+    # avg loss
+    train_loss = train_loss / len(trainloader.sampler)
+    valid_loss = train_loss / len(validloader.sampler)
+
+    # print training/validation statistics 
+    print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+        epochs, train_loss, valid_loss))
+    
+    # save model if validation loss has decreased
+    if valid_loss <= valid_loss_min:
+        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
+        valid_loss_min, valid_loss))
+
+        torch.save(model.state_dict(), 'model_plain_chess.pt')
+        valid_loss_min = valid_loss
+
+
+#      ___           ___           ___           ___     
+#     /\  \         /\  \         /\  \         /\  \    
+#     \:\  \       /::\  \       /::\  \        \:\  \   
+#      \:\  \     /:/\:\  \     /:/\ \  \        \:\  \  
+#      /::\  \   /::\~\:\  \   _\:\~\ \  \       /::\  \ 
+#     /:/\:\__\ /:/\:\ \:\__\ /\ \:\ \ \__\     /:/\:\__\
+#    /:/  \/__/ \:\~\:\ \/__/ \:\ \:\ \/__/    /:/  \/__/
+#   /:/  /       \:\ \:\__\    \:\ \:\__\     /:/  /     
+#   \/__/         \:\ \/__/     \:\/:/  /     \/__/      
+#                  \:\__\        \::/  /                 
+#                   \/__/         \/__/            
+
+
+def test(model, model_name, criterion):
+
+    model.loard_state_dict(torch.load(model_name))
+
+    test_loss = 0.0
+
+    model.eval()
+
+    for data, target in testloader:
+
+        # forward
+        output = model(data)
+        # batch loss
+        loss = criterion(output, target)
+        # train_loss update
+        test_loss += loss.item()*data.size(0)
+
+        # TO DO comparison to true data
+        # with conversion of output to wanted format
+
+
+    # average test loss
+    test_loss = test_loss/len(testloader.dataset)
+    print('Test Loss: {:.6f}\n'.format(test_loss))
