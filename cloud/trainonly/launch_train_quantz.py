0eb13a2cb6877b3ccf6c90dbdac7ac00c3d9a742
MikeHatchi <mikehatchi@hotmail.com>
2023-07-28 15:40:36 +0200

    cleansing launch_train

--- cloud/trainonly/launch_train_quantz.py
+++ cloud/trainonly/launch_train_quantz.py
@@ -5,20 +5,27 @@ from torch.utils.data import DataLoader
 import numpy as np
 import pandas as pd
 
-#sys.path.insert(1,"code_src/model_src/clear/")
-#sys.path.insert(1,"code_src/model_src/quantz/")
+"""
+Training Only here
+Recall for TESTING, please run compile_fhe.py
+"""
 
 #source
-#from train_v3_source_quantz import train_valid, test
-#from dataset_v3_source import Chessset
-#from cnn_v13_44bit_source_quantz import QTChessNET
+from train_source_quantz import train_valid
+
+sys.path.append("model_src/")
+from dataset_source import Chessset
 
 # target
-from train_v3_target_quantz import train_valid
-from dataset_v3_target import Chessset
+#from train_target_quantz import train_valid
+#from dataset_target import Chessset
 
-sys.path.insert(1,"model_src/quantz/")
-from target_44cnn_quantz import QTtrgChessNET
+sys.path.append("model_src/quantz/")
+# source
+from source_44cnn_quantz import QTChessNET
+
+# target (for training only) 
+#from target_44cnn_quantz import QTtrgChessNET # CAUTION "target_44cnn_quantz_EVAL" is for Inference
 
 """
 LOADING SECTION
@@ -65,18 +72,11 @@ valid_loader = DataLoader(validset, batch_size = 64, shuffle=True, drop_last=Tru
 test_loader = DataLoader(testset, batch_size = 64, shuffle=True, drop_last=True)
 
 #model
-#model = QTChessNET()
-model = QTtrgChessNET()
+model = QTChessNET()
+#model = QTtrgChessNET()
 
 #loss
 criterion = nn.MSELoss()
 
 ## TRAINING
 train_valid(model, train_loader, valid_loader, criterion)
-
-## TESTING
-### model with lowest validation loss
-#model.load_state_dict(torch.load("/content/gdrive/MyDrive/FHX/weigths/source_model_plain_chess4.pt"))
-
-# Test and accuracy
-#test(model, test_loader, criterion)
