b343c0c18cc20f8375da9ec3cb4e97341ff61b51
MikeHatchi <mikehatchi@hotmail.com>
2023-10-13 14:09:40 +0200

    add algebraic bitboard hash table

--- client_local/chess_env/base.py
+++ client_local/chess_env/base.py
@@ -16,7 +16,8 @@ cb_rows = 8                     #number of rows
 sqsize = sp_width // cb_cols    #size of squares
 
 
-"""Python-Chess lib. uses alphanumeric coordinates (e2 equivalent to col 1 - row 2) but also
+"""
+Python-Chess lib. uses alphanumeric coordinates (e2 equivalent to col 1 - row 2) but also
 bitboard logic: an array of square table location within chessboard.
 the move e2e4 becomes 8 to 24.
 """
@@ -30,3 +31,27 @@ bitboard = np.array([
     [8,9,10,11,12,13,14,15],
     [0,1,2,3,4,5,6,7],
     ])
+
+"""
+Hash tables:
+- dict of square_alphanum (square: alphanumeric),
+- dict of alphanum_square (alphanumeric: square).
+
+col = ["a", "b", "c", "d", "e", "f", "g", "h"]
+row = [1,2,3,4,5,6,7,8]
+bitalpha = []
+square_alphanum = {}
+
+for ic in np.ndenumerate(bitboard):
+    square_alphanum[ic[1]] = "%s%s" %(col[ic[0][1]],row[7-ic[0][0]])
+
+alphanum_square = {v: k for k, v in square_alphanum.items()}
+"""
+alphanum_square = {'a8': 56, 'b8': 57, 'c8': 58, 'd8': 59, 'e8': 60, 'f8': 61, 'g8': 62, 'h8': 63,
+                   'a7': 48, 'b7': 49, 'c7': 50, 'd7': 51, 'e7': 52, 'f7': 53, 'g7': 54, 'h7': 55,
+                   'a6': 40, 'b6': 41, 'c6': 42, 'd6': 43, 'e6': 44, 'f6': 45, 'g6': 46, 'h6': 47,
+                   'a5': 32, 'b5': 33, 'c5': 34, 'd5': 35, 'e5': 36, 'f5': 37, 'g5': 38, 'h5': 39,
+                   'a4': 24, 'b4': 25, 'c4': 26, 'd4': 27, 'e4': 28, 'f4': 29, 'g4': 30, 'h4': 31,
+                   'a3': 16, 'b3': 17, 'c3': 18, 'd3': 19, 'e3': 20, 'f3': 21, 'g3': 22, 'h3': 23,
+                   'a2': 8, 'b2': 9, 'c2': 10, 'd2': 11, 'e2': 12, 'f2': 13, 'g2': 14, 'h2': 15,
+                   'a1': 0, 'b1': 1, 'c1': 2, 'd1': 3, 'e1': 4, 'f1': 5, 'g1': 6, 'h1': 7,}
