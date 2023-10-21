c1aed0a0225ef2b9bbc0748c3fe8ee8a8545d89c
MikeHatchi <mikehatchi@hotmail.com>
2023-10-21 10:04:12 +0200

    add questions

--- server_cloud/dev_ab_eval/alpha_beta.py
+++ server_cloud/dev_ab_eval/alpha_beta.py
@@ -3,20 +3,22 @@ Analysis chosen move in 1st place
 
 pseudo code
 
+Which input_data?
 Alpha Prune
 if Type(Child) = Min # minimizer:
     if Uncle(Child) and Weight(Uncle):
         if Weight(Child) <= Weight(Uncle):
             Pruning
-            Weight(Dad) = Weight(Child)
-        
+            Weight(Dad) = Weight(Child) NNUE Which input_data?
+Which output_data?
+
 Beta Prune
 if Type(Child) = Max # maximizer:
     if Uncle(Child) and Weight(Uncle):
         if Weight(Child) >= Weight(Uncle):
             Pruning
-            Weight(Dad) = Weight(Child)
-
+            Weight(Dad) = Weight(Child) NNUE
+Which output_data?
 
 TRI MEMORY PRUNE_MOVE GOOD_MOVE_SEQUENCE PATTERNS
 Depth 1: Heuristic sorting
